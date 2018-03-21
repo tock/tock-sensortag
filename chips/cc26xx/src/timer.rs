6bb79d77138f77632df7775e0bf53958322e7dbc
Filip <filip.nilsson@blixtmail.se>
2018-03-21 14:52:16 +0100

    cc26xx: timer: Use new reg-interface in timer functions
    (cherry picked from commit 1a38f54)

--- chips/cc26xx/src/timer.rs
+++ chips/cc26xx/src/timer.rs
@@ -4,18 +4,18 @@ use prcm;
 
 #[repr(C)]
 pub struct Registers {
-    pub cfg: ReadWrite<u32>,
-    pub tamr: ReadWrite<u32>,
+    pub cfg: ReadWrite<u32, Configuration::Register>,
+    pub tamr: ReadWrite<u32, TimerAMode::Register>,
     pub tbmr: ReadWrite<u32>,
-    pub ctl: ReadWrite<u32>,
+    pub ctl: ReadWrite<u32, Control::Register>,
     pub sync: ReadWrite<u32>,
 
     _reserved0: [u8; 0x4],
 
-    pub imr: ReadWrite<u32>,
+    pub imr: ReadWrite<u32, InterruptMask::Register>,
     pub ris: ReadOnly<u32>,
-    pub mis: ReadOnly<u32>,
-    pub iclr: ReadWrite<u32>,
+    pub mis: ReadOnly<u32, MaskedInterruptStatus::Register>,
+    pub iclr: ReadWrite<u32, InterruptClear::Register>,
     pub tailr: ReadWrite<u32>,
     pub tbilr: ReadWrite<u32>,
     pub tamatchr: ReadWrite<u32>,
@@ -43,6 +43,39 @@ pub struct Registers {
     pub andccp: ReadWrite<u32>,
 }
 
+register_bitfields![
+    u32,
+    Control [
+        TAEN OFFSET(0) NUMBITS(1) [],
+        TBEN OFFSET(8) NUMBITS(1) []
+    ],
+    Configuration [
+        CFG OFFSET(0) NUMBITS(3) [
+            timer32Bit = 0x0,
+            timer64Bit = 0x4
+        ]
+    ],
+    TimerAMode [
+        TAMR OFFSET(0) NUMBITS(2) [
+            OneShot = 0x1,
+            Periodic = 0x2,
+            Capture = 0x3
+        ]
+    ],
+    InterruptMask [
+        TATOIM OFFSET(0) NUMBITS(1) [],
+        TBTOIM OFFSET(8) NUMBITS(1) []
+    ],
+    MaskedInterruptStatus [
+        TATOMIS OFFSET(0) NUMBITS(1) [],
+        TBTOMIS OFFSET(8) NUMBITS(1) []
+    ],
+    InterruptClear [
+        TATOCINT OFFSET(0) NUMBITS(1) [],
+        TBTOCINT OFFSET(8) NUMBITS(1) []
+    ]
+];
+
 pub const GPT_CFG_32_BIT: u32 = 0x0;
 pub const GPT_ONE_SHOT: u32 = 0x1;
 pub const GPT_REG_BIT: u32 = 0x1;
@@ -62,7 +95,6 @@ pub static mut GPT3: Timer = Timer::new(TimerBase::GPT3);
 
 pub struct Timer {
     regs: *const Registers,
-    reg_bit: u32,
     client: Cell<Option<&'static TimerClient>>,
 }
 
@@ -80,7 +112,6 @@ impl Timer {
     const fn new(gpt_base: TimerBase) -> Timer {
         Timer {
             regs: (gpt_base as u32) as *const Registers,
-            reg_bit: GPT_REG_BIT,
             client: Cell::new(None),
         }
     }
@@ -89,26 +120,26 @@ impl Timer {
         let regs: &Registers = unsafe { &*self.regs };
 
         // Disable timer before configuration
-        regs.ctl.set(regs.ctl.get() & !self.reg_bit);
-        regs.cfg.set(GPT_CFG_32_BIT);
+        regs.ctl.modify(Control::TAEN::CLEAR);
+        regs.cfg.write(Configuration::CFG::timer64Bit);
 
         // Set type and initial timer value
-        regs.tamr.set(GPT_ONE_SHOT);
+        regs.tamr.write(TimerAMode::TAMR::OneShot);
         regs.tailr.set(value);
 
         // Enable interrupts and start the timer
-        regs.imr.set(self.reg_bit);
-        regs.ctl.set(regs.ctl.get() | self.reg_bit);
+        regs.imr.write(InterruptMask::TATOIM::SET);
+        regs.ctl.modify(Control::TAEN::SET);
     }
 
     pub fn has_fired(&self) -> bool {
         let regs: &Registers = unsafe { &*self.regs };
-        (regs.mis.get() & self.reg_bit) != 0
+        regs.mis.is_set(MaskedInterruptStatus::TATOMIS)
     }
 
     pub fn handle_interrupt(&self) {
         let regs: &Registers = unsafe { &*self.regs };
-        regs.iclr.set(regs.iclr.get() | self.reg_bit);
+        regs.iclr.modify(InterruptClear::TATOCINT::SET);
         self.client.get().map(|client| {
             client.fired();
         });
