3a8a577651db7363a134d42acc5528763b66f507
Filip <filip.nilsson@blixtmail.se>
2018-04-14 19:09:27 +0200

    cc26x0: power_manager: Refactored code and cleaned up comments

--- chips/cc26x0/src/power_manager.rs
+++ chips/cc26x0/src/power_manager.rs
@@ -1,53 +1,43 @@
 /// Power Manager
 ///
-/// Generalised power management for peripherals. It handles toggling
-/// their power on request & release once they've been used.
-///
+/// Generalised power management for peripherals.
 // TODO(cpluss): complete documentation above
 
 use core::cell::Cell;
 use kernel::common::{List, ListLink, ListNode};
 
-/// PoweredClient
-///     A peripheral which is powered (almost all) and manages
-///     its own requests of powers - to be used in conjunction with a
-///     PoweredClient.
+/// A peripheral which knows how to power itself up or down.
+/// Must be used in conjunction with a PoweredClient.
 ///
-///     NOTE: you need to register all Powered Peripherals
-///           during initialisation.
+/// NOTE: you need to register all Powered Peripherals during initialisation.
 pub trait PoweredClient {
-    /// Identifier for this peripheral. Used to distinguish
-    /// peripherals between each other.
+    /// Identifier for this peripheral. Used to distinguish peripherals between each other.
     fn identifier(&self) -> u32;
 
-    /// Power on this peripheral
+    /// Power on the peripheral
     fn power_on(&self);
 
-    /// Power off this peripheral
+    /// Power off the peripheral
     fn power_off(&self);
 
-    /// This is invoked before the chip goes into sleep mode, if the
-    /// peripheral is powered.
+    /// This is invoked before the chip goes into sleep mode, if the peripheral is powered.
     fn before_sleep(&self);
 
     /// This is invoked once the chip has woken up from any sleep mode.
     fn after_wakeup(&self);
 
-    /// This is the lowest sleep mode this module will still be able
-    /// to function in.
+    /// This is the lowest sleep mode this module will still be able to function in.
     fn lowest_sleep_mode(&self) -> u32;
 }
 
 pub struct PoweredPeripheral<'a> {
     client: Cell<Option<&'a PoweredClient>>,
     next: ListLink<'a, PoweredPeripheral<'a>>,
-    usage_count: Cell<u32>,
+    usage: Cell<u32>,
 }
 
 impl<'a> ListNode<'a, PoweredPeripheral<'a>> for PoweredPeripheral<'a> {
-    fn next(&self) -> &'a ListLink<PoweredPeripheral<'a>> {
-        &self.next
-    }
+    fn next(&self) -> &'a ListLink<PoweredPeripheral<'a>> { &self.next }
 }
 
 impl<'a> PoweredPeripheral<'a> {
@@ -55,38 +45,23 @@ impl<'a> PoweredPeripheral<'a> {
         PoweredPeripheral {
             client: Cell::new(Some(client)),
             next: ListLink::empty(),
-            usage_count: Cell::new(0),
+            usage: Cell::new(0),
         }
     }
 
-    pub fn client(&self) -> &'a PoweredClient {
-        self.client.get().expect("")
-    }
+    pub fn client(&self) -> &'a PoweredClient { self.client.get().expect("") }
 
-    pub fn usage_map<F>(&self, closure: F)
-    where
-        F: FnOnce(u32) -> u32,
-    {
-        let val = self.usage_count.get();
-        self.usage_count.set(closure(val))
-    }
+    pub fn usage_count(&self) -> u32 { self.usage.get() }
 
-    pub fn usage(&self) -> u32 {
-        self.usage_count.get()
-    }
+    pub fn increment_usage(&self) { self.usage.set(self.usage.get() - 1); }
+
+    pub fn decrement_usage(&self) { self.usage.set(self.usage.get() + 1); }
 }
 
-/// Power manager
-///     Responsible to keep track of what peripherals
-///     is used, and powered on. As well as to power
-///     on and off specific peripherals.
-///
-///     It also has the functionality to power off
-///     all used peripherals once the MCU is ready
-///     to go to sleep mode, as it's needed in many
-///     cases.
+/// Keeps track of what peripherals are being used and if they should be powered on.
+/// Also manages sleep modes.
 pub struct PowerManager<'a> {
-    /// A list of IDs for powered peripherals
+    /// A list of IDs for powered peripherals.
     powered_peripherals: List<'a, PoweredPeripheral<'a>>,
 }
 
@@ -94,53 +69,43 @@ impl<'a> PowerManager<'a> {
     pub const fn new() -> PowerManager<'a> {
         PowerManager {
             powered_peripherals: List::new(),
-            //_chip: Cell::new(None),
         }
     }
 
-    /// Register a powered peripheral to hook up and be notified
-    /// when specific events occur (sleep, etc).
+    /// Register a powered peripheral to be notified when going into sleep mode or waking up.
     pub fn register(&self, peripheral: &'a PoweredPeripheral<'a>) {
         self.powered_peripherals.push_head(peripheral);
     }
 
-    /// Request access for a specific peripheral to be used
+    /// Request access for a specific peripheral to be used.
     pub fn request(&self, identifier: u32) {
-        let powered_peripheral = self.powered_peripherals
+        let peripheral = self.powered_peripherals
             .iter()
             .find(|p| p.client().identifier() == identifier)
             .expect("peripheral requested that has not been registered.");
 
-        if powered_peripheral.usage() == 0 {
-            powered_peripheral.client().power_on();
-        }
+        if peripheral.usage_count() == 0 { peripheral.client().power_on(); }
 
-        powered_peripheral.usage_map(|usage: u32| usage + 1);
+        peripheral.increment_usage();
     }
 
-    /// Release a specific peripheral as no longer being used
+    /// Release a specific peripheral as no longer being used.
     pub fn release(&self, identifier: u32) {
-        let powered_peripheral = self.powered_peripherals
+        let peripheral = self.powered_peripherals
             .iter()
             .find(|p| p.client().identifier() == identifier)
             .expect("peripheral requested that has not been registered.");
 
-        powered_peripheral.usage_map(|usage: u32| {
-            if usage > 0 {
-                usage - 1
-            } else {
-                0
-            }
-        });
+        if peripheral.usage_count() > 0 { peripheral.decrement_usage() }
 
-        if powered_peripheral.usage() == 0 {
-            powered_peripheral.client().power_off();
+        if peripheral.usage_count() == 0 {
+            peripheral.client().power_off();
         }
     }
 
     pub fn prepare_sleep(&self) {
         for peripheral in self.powered_peripherals.iter() {
-            if peripheral.usage() > 0 {
+            if peripheral.usage_count() > 0 {
                 peripheral.client().before_sleep();
                 peripheral.client().power_off();
             }
@@ -149,12 +114,10 @@ impl<'a> PowerManager<'a> {
 
     pub fn after_wakeup(&self) {
         for peripheral in self.powered_peripherals.iter() {
-            if peripheral.usage() > 0 {
+            if peripheral.usage_count() > 0 {
                 peripheral.client().power_on();
                 peripheral.client().after_wakeup();
             }
         }
     }
-
-
 }
