567917737543d3c73bfda542200ed93ce294e19c
Filip <filip.nilsson@blixtmail.se>
2018-04-16 14:59:21 +0200

    cc26x0: peripheral_manager: Added documentation

--- chips/cc26x0/src/peripheral_manager.rs
+++ chips/cc26x0/src/peripheral_manager.rs
@@ -1,12 +1,14 @@
 use core::cell::Cell;
 use kernel::common::{List, ListLink, ListNode};
 
+/// A PowerClient implements ways to get notified when the chip changes its power mode.
 pub trait PowerClient {
     fn before_sleep(&self, sleep_mode: u32);
     fn after_wakeup(&self, sleep_mode: u32);
     fn lowest_sleep_mode(&self) -> u32;
 }
 
+/// Wrapper around PowerClient to be used in a linked list.
 pub struct Peripheral<'a> {
     client: Cell<Option<&'a PowerClient>>,
     next: ListLink <'a, Peripheral<'a>>
@@ -20,17 +22,20 @@ impl<'a> Peripheral<'a> {
         }
     }
 
+    /// Returns the lowest possible power mode this peripheral can enter at the moment.
     pub fn lowest_sleep_mode(&self) -> u32 {
         self.client.get()
             .map(|c| c.lowest_sleep_mode())
             .expect("No power client for a peripheral is set.")
     }
 
+    /// Prepares the peripheral before going into sleep mode.
     pub fn before_sleep(&self, sleep_mode: u32) {
         self.client.get()
             .map(|c| c.before_sleep(sleep_mode));
     }
 
+    /// Initializes the peripheral after waking up from sleep mode.
     pub fn after_wakeup(&self, sleep_mode: u32) {
         self.client.get()
             .map(|c| c.after_wakeup(sleep_mode));
@@ -41,6 +46,7 @@ impl<'a> ListNode<'a, Peripheral<'a>> for Peripheral<'a> {
     fn next(&self) -> &'a ListLink<Peripheral<'a>> { &self.next }
 }
 
+/// Manages peripherals wanting to get notified when changing power modes.
 pub struct PeripheralManager<'a> {
     peripherals: List<'a, Peripheral<'a>>,
 }
@@ -52,22 +58,26 @@ impl<'a> PeripheralManager<'a> {
         }
     }
 
+    /// Registers a new peripheral to be managed by the PeripheralManager.
     pub fn register_peripheral(&self, peripheral: &'a Peripheral<'a>) {
         self.peripherals.push_head(peripheral);
     }
 
+    /// Prepares all registered clients for entering sleep mode.
     pub fn before_sleep(&self, sleep_mode: u32) {
         for peripheral in self.peripherals.iter() {
             peripheral.before_sleep(sleep_mode);
         }
     }
 
+    /// Starts all registered clients after waking up from sleep mode.
     pub fn after_wakeup(&self, sleep_mode: u32) {
         for peripheral in self.peripherals.iter() {
             peripheral.after_wakeup(sleep_mode);
         }
     }
 
+    /// Returns the lowest possible power mode allowed by the registered clients.
     pub fn lowest_sleep_mode(&self) -> u32 {
         self.peripherals.iter().fold(0, |prev, peripheral| {
             prev.max(peripheral.lowest_sleep_mode())
--- chips/cc26x0/src/power_manager.rs
+++ chips/cc26x0/src/power_manager.rs
@@ -1,59 +1,59 @@
-/// Power Manager
-///
-/// This construct facilitates the management of different hardware resources on
-/// on a chip related to power. By keeping track of the number of references to a certain resource,
-/// we can decide if the resource needs to be powered on or not.
-///
-/// For a resource to be tracked it needs to be registered with the power manager.
-/// The power manager then controls all registered resources through a resource manager.
-/// The resource manager knows the hardware specific details of how to enable/disable the resources.
-///
-/// Following is an example from the cc26xx family of microcontrollers that shows how different
-/// power regions can be controlled through the power manager.
-///
-/// ''' rust
-/// /// All requests to use a certain power region goes through this power manager.
-/// pub static mut PM: PowerManager<RegionManager> = PowerManager::new(RegionManager);
-///
-/// /// Used to power on/off different power regions.
-/// pub struct RegionManager;
-///
-/// impl ResourceManager for RegionManager {
-///    fn enable_resource(&self, resource_id: u32) {
-///        let domain = PowerDomain::from(resource_id);
-///        Power::enable_domain(domain);
-///    }
-///
-///    fn disable_resource(&self, resource_id: u32) {
-///        let domain = PowerDomain::from(resource_id);
-///        Power::disable_domain(domain);
-///    }
-/// }
-///
-/// /// Registers all resources we want the power manager to keep track off.
-/// pub unsafe fn init() {
-///    for pwr_region in POWER_REGIONS.iter() {
-///        // pwer_region is of type Resource and has a unique id associated with it.
-///        PM.register_resource(&pwr_region);
-///    }
-/// }
-/// '''
-///
-/// A peripheral that wants to use a certain power region then simply requests the resource through
-/// the power manager and releases it once it is done.
-///
-/// ''' rust
-/// PM.request_resource(power_region_id);
-///
-/// // Do some work which requires the power region to be on.
-///
-/// PM.release_resource(power_region_id);
-/// '''
+//! Power Manager
+//!
+//! This construct facilitates the management of different hardware resources on
+//! on a chip related to power. By keeping track of the number of references to a certain resource,
+//! we can decide if the resource needs to be powered on or not.
+//!
+//! For a resource to be tracked it needs to be registered with the power manager.
+//! The power manager then controls all registered resources through a resource manager.
+//! The resource manager knows the hardware specific details of how to enable/disable the resources.
+//!
+//! Following is an example from the cc26xx family of microcontrollers that shows how different
+//! power regions can be controlled through the power manager.
+//!
+//! ``` rust
+//! /// All requests to use a certain power region goes through this power manager.
+//! pub static mut PM: PowerManager<RegionManager> = PowerManager::new(RegionManager);
+//!
+//! /// Used to power on/off different power regions.
+//! pub struct RegionManager;
+//!
+//! impl ResourceManager for RegionManager {
+//!    fn enable_resource(&self, resource_id: u32) {
+//!        let domain = PowerDomain::from(resource_id);
+//!        Power::enable_domain(domain);
+//!    }
+//!
+//!    fn disable_resource(&self, resource_id: u32) {
+//!        let domain = PowerDomain::from(resource_id);
+//!        Power::disable_domain(domain);
+//!    }
+//! }
+//!
+//! /// Registers all resources we want the power manager to keep track off.
+//! pub unsafe fn init() {
+//!    for pwr_region in POWER_REGIONS.iter() {
+//!        // pwer_region is of type Resource and has a unique id associated with it.
+//!        PM.register_resource(&pwr_region);
+//!    }
+//! }
+//! ```
+//!
+//! A peripheral that wants to use a certain power region then simply requests the resource through
+//! the power manager and releases it once it is done.
+//!
+//! ``` rust
+//! PM.request_resource(power_region_id);
+//!
+//! // Do some work which requires the power region to be on.
+//!
+//! PM.release_resource(power_region_id);
+//! ```
 
 use core::cell::Cell;
 use kernel::common::{List, ListLink, ListNode};
 
-/// Manages the details of how resources are enabled and disabled
+/// Manages the details of how resources are enabled and disabled.
 pub trait ResourceManager {
     fn enable_resource(&self, resource_id: u32);
     fn disable_resource(&self, resource_id: u32);
@@ -79,12 +79,12 @@ impl<'a> Resource<'a> {
         }
     }
 
-    /// Increment the number of references to this resource
+    /// Increment the number of references to this resource.
     pub fn inc_ref_count(&self) {
         self.ref_count.set(self.ref_count.get() + 1);
     }
 
-    /// Decrement the number of references to this resource
+    /// Decrement the number of references to this resource.
     pub fn dec_ref_count(&self) {
         self.ref_count.set(self.ref_count.get() - 1);
     }
