fe22c64af6b043470c1b0fceb32a2b953e18c9c3
sebastian <me@sebastianlund.se>
2018-04-16 14:53:03 +0200

    cc26x0: implement experimental sleep
    Still a long way to go, but this is a good start.
    This will not work without https://github.com/helena-project/tock/pull/885

--- chips/cc26x0/src/chip.rs
+++ chips/cc26x0/src/chip.rs
@@ -1,6 +1,7 @@
 use cortexm3::{self, nvic};
 use cc26xx::gpio;
 use cc26xx::peripheral_interrupts::*;
+use kernel::common::regs::ReadWrite;
 
 const X0_RF_CPE1: u32 = 2;
 const X0_RF_CPE0: u32 = 9;
@@ -11,11 +12,50 @@ use timer;
 use uart;
 use kernel;
 use rtc;
+use prcm;
+use kernel::support;
+use peripherals;
+use aux;
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub enum SleepMode {
+    DeepSleep = 0,
+    Sleep = 1,
+    Active = 2,
+}
+
+impl From<u32> for SleepMode {
+    fn from(n: u32) -> Self {
+        match n {
+            0 => SleepMode::DeepSleep,
+            1 => SleepMode::Sleep,
+            2 => SleepMode::Active,
+            _ => unimplemented!()
+        }
+    }
+}
+
+pub struct SystemControlRegisters {
+    scr: ReadWrite<u32, SystemControl::Register>,
+}
+
+register_bitfields![
+    u32,
+    SystemControl [
+        SLEEP_ON_EXIT   OFFSET(1)   NUMBITS(1) [], // Go to sleep after ISR
+        SLEEP_DEEP      OFFSET(1)   NUMBITS(1) [], // Enable deep sleep
+        SEVONPEND       OFFSET(1)   NUMBITS(1) []  // Wake up on all events (even disabled interrupts)
+    ]
+];
 
 pub struct Cc26x0 {
     mpu: (),
     systick: cortexm3::systick::SysTick,
+    sys_ctrl_regs: *const SystemControlRegisters,
 }
+
+const SYS_CTRL_BASE: u32 = 0xE000ED10;
 
 impl Cc26x0 {
     pub unsafe fn new() -> Cc26x0 {
@@ -23,6 +63,7 @@ impl Cc26x0 {
             mpu: (),
             // The systick clocks with 48MHz by default
             systick: cortexm3::systick::SysTick::new_with_calibration(48 * 1000000),
+            sys_ctrl_regs: SYS_CTRL_BASE as *const SystemControlRegisters,
         }
     }
 }
@@ -77,5 +118,47 @@ impl kernel::Chip for Cc26x0 {
         unsafe { nvic::has_pending() }
     }
 
-    fn prepare_for_sleep(&self) { }
+    fn sleep(&self) {
+        unsafe {
+            let sleep_mode: SleepMode = SleepMode::from(peripherals::M.lowest_sleep_mode());
+            let regs = &*self.sys_ctrl_regs;
+
+            match sleep_mode {
+                SleepMode::DeepSleep => {
+                    peripherals::M.before_sleep(sleep_mode as u32);
+
+                    // Power down the AUX power
+                    aux::AUX_CTL.power_down();
+
+                    // Power down the MCU, it is no longer needed
+                    // until we wake up again.
+                    prcm::Power::disable_domain(prcm::PowerDomain::CPU);
+                    prcm::mcu_power_down();
+
+                    // Set the deep sleep bit
+                    regs.scr.modify(SystemControl::SLEEP_DEEP::SET);
+
+                    // TODO(cpluss): perform power down of regions & clocks
+                    // TODO(cpluss): check VIMS mode, it needs to be OFF
+                },
+                _ => ()
+            }
+
+            support::wfi();
+
+            match sleep_mode {
+                SleepMode::DeepSleep => {
+                    peripherals::M.after_wakeup(sleep_mode as u32);
+
+                    // Clear the deep sleep bit
+                    regs.scr.modify(SystemControl::SLEEP_DEEP::CLEAR);
+
+                    prcm::Power::enable_domain(prcm::PowerDomain::CPU);
+
+                    // TODO(cpluss): perform power up of regions & clocks
+                },
+                _ => ()
+            }
+        }
+    }
 }
