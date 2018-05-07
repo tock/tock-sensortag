6d1a4e39bc14d008b72ec0f511b261ee28f81086
Daniel Cellucci <dwcellucci@gmail.com>
2018-05-06 21:07:50 -0700

    tried to implement register changes

--- chips/cc26x0/src/udma.rs
+++ chips/cc26x0/src/udma.rs
@@ -3,6 +3,7 @@
 ///
 
 use kernel::common::regs::{ReadOnly, ReadWrite, WriteOnly};
+use kernel::ReturnCode;
 use prcm;
 
 pub const UDMA_BASE: usize = 0x4002_0000;
@@ -12,24 +13,49 @@ pub struct ControlTable {
     config_array: [DMAConfig; 32]
 }
 
-
-#[derive(Clone, Copy)]
+#[derive(Copy, Clone)]
 struct DMAConfig {
     source_ptr: usize,
     dest_ptr: usize,
-    control: usize,
+    control: ReadWrite<u32,DMATableControl::Register>,
     _unused: usize,
 }
+
+#[allow(non_camel_case_types)]
+#[derive(Copy, Clone, PartialEq)]
+#[repr(u8)]
+pub enum DMAPeripheral {
+    SOFTWARE_0 = 0,
+    UART0_RX = 1,
+    UART0_TX = 2,
+    SSP0_RX = 3,
+    SSP0_TX = 4,
+    AUX_ADC = 7,
+    AUX_SW = 8,
+    GPT0_A = 9,
+    GPT0_B = 10,
+    GPT1_A = 11,
+    GPT1_B = 12,
+    AON_PROG2 = 13,
+    DMA_PROG = 14,
+    AON_RTC = 15,
+    SSP1_RX = 16,
+    SSP1_TX = 17,
+    SOFTWARE_1 = 18,
+    SOFTWARE_2 = 19,
+    SOFTWARE_3 = 20,
+}
+
 
 static mut DMACTRLTAB: ControlTable = ControlTable::new();
 
 impl ControlTable {
     const fn new() -> ControlTable {
         ControlTable {
-            config_array: [ DMAConfig {
+            config_array: [DMAConfig {
                                 source_ptr: 0, 
                                   dest_ptr: 0,
-                                   control: 0,
+                                   control: ReadWrite::new(0),
                                    _unused: 0
                             }; 32]
         }
@@ -72,6 +98,38 @@ register_bitfields! [u32,
     ],
     Control [
         BASEPTR OFFSET(10) NUMBITS(22)
+    ], 
+    DMATableControl [
+        TRANSFERSIZE OFFSET(4) NUMBITS(10) [],
+        ARB OFFSET(14) NUMBITS(4) [
+            ARB1    = 0,
+            ARB2    = 1,
+            ARB4    = 2, 
+            ARB8    = 3,
+            ARB16   = 4,
+            ARB32   = 5,
+            ARB64   = 6,
+            ARB128  = 7,
+            ARB256  = 8,
+            ARB512  = 9,
+            ARB1024 = 10
+        ],
+        PSIZE OFFSET(24) NUMBITS(6) [
+            PSIZE8  = 0,
+            PSIZE16 = 1,
+            PSIZE32 = 2
+        ],
+        SRCINC OFFSET(26) NUMBITS(2) [
+            INC8    = 0,
+            INC16   = 1,
+            INC32   = 2,
+            INC0    = 3
+        ],
+        DSTINC OFFSET(30) NUMBITS(2) [
+            INC8    = 0,
+            INC16   = 17,
+            INC32   = 34
+        ]
     ]
 ];
 
@@ -89,6 +147,10 @@ impl Udma {
         }
     }
 
+    /// Enable Function
+    /// Sets up the power domain and the clocks
+    /// It then sets MASTERNEABLE in the CFG register and writes the location of
+    /// the control table in the CTRL register.
     pub fn enable(
         &self,
     ) {
@@ -104,5 +166,25 @@ impl Udma {
         prcm::Power::enable_domain(prcm::PowerDomain::Peripherals);
         while !prcm::Power::is_enabled(prcm::PowerDomain::Peripherals) {}
         prcm::Clock::enable_dma();
+    }
+
+    fn set_dest_pointer(
+        &self, 
+        channel: DMAPeripheral,
+        dest_address: usize,
+    ) { 
+        unsafe{
+            DMACTRLTAB.config_array[channel as usize].dest_ptr = dest_address;
+        }
+    }
+
+    fn set_source_pointer(
+        &self, 
+        channel: DMAPeripheral,
+        source_address: usize,
+    ) { 
+        unsafe{
+            DMACTRLTAB.config_array[channel as usize].source_ptr = source_address;
+        }
     }
 }
